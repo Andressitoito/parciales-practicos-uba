3c7c5d50d0231bc94a5590e838852bf4911cef02
Andresito <60946504+Andressitoito@users.noreply.github.com>
2024-05-04 16:07:42 -0300

    Corregido Error de 'Missing Key Prop'
    Se solucionó el problema de 'missing key prop' en el componente principal 'page.js'. Se agregaron las propiedades de clave faltantes para garantizar un rendimiento óptimo y evitar errores en la renderización del componente.

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Parciales() {
    <div className="flex flex-col items-center justify-center h-screen">
     <h2 className="text-white text-5xl mb-8 font-bold">Practica Examenes ICSE</h2>
     {years.map(year => (
-     <div className="w-48 mb-4">
+     <div key={year} className="w-48 mb-4">
       <Link href={`/parciales/${year}`}>
        <div className="cursor-pointer bg-gray-800 hover:bg-gray-700 text-white py-2 px-4 rounded-lg transition duration-300 text-center">
         {year}
