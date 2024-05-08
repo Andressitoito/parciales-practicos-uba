de2ffde637ced91220ecc5c940aecde6d335e9f8
Andresito <60946504+Andressitoito@users.noreply.github.com>
2024-05-07 22:08:51 -0300

    Configuración de la Estructura de Carpetas para MongoDB
    Se está estableciendo la estructura de carpetas necesaria para trabajar con MongoClient sin el uso de Mongoose. Este paso es crucial para garantizar una organización adecuada y una integración fluida con la base de datos MongoDB.

---
+++ src/pages/api/database/get_database_data.js
@@ -0,0 +1,28 @@
+import { getData } from "../../../../lib/mongodb/data/getData";
+
+async function handler(req, res) {
+ if (req.method === "GET") {
+
+  let documents
+  try {
+   documents = await getData();
+  } catch (error) {
+   console.log("error ", error);
+
+   return res.status(500).json({
+    status: 500,
+    message: "Something went wrong in the server connection",
+    error: error.toString(),
+   });
+  }
+  
+  res.status(200).json({
+   status: 200,
+   message: "Get all data successfully",
+   documents,
+  });
+  
+ }
+}
+
+export default handler;
