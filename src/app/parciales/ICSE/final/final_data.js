0c44c992b52a1328656bc564894e4ecbe174d2b7
Andresito <60946504+Andressitoito@users.noreply.github.com>
2024-06-22 21:49:10 -0300

    Actualizacion PARTE B
    Se actualizo la parte B del final con las preguntas actuales.

--- src/app/parciales/ICSE/final/final_data.js
+++ src/app/parciales/ICSE/final/final_data.js
@@ -227,7 +227,234 @@ export const final_data = [
 		year: 2023,
 		name: "PARTE B",
 		questions: [
-	
+			{
+				"question": "Según Luis A. Romero, desde 2001 y durante 2002, la crisis se desplegó en todo su alcance debido a la conjugación de diversas crisis. Entre ellas:",
+				"answers": [
+					"A. La crisis política por el cuestionamiento general a la legitimidad de los gobernantes.",
+					"B. La crisis económica provocada por la impopular intervención del FMI en la economía argentina.",
+					"C. La crisis global como producto del fin violento de la Guerra Fría generaba un contexto incierto y potencias interesadas en otros asuntos.",
+					"D. La crisis política derivada de la acefalía presidencial.",
+					"E. Las opciones correctas son la A y la D.",
+					"F. Las opciones correctas son la B y la C.",
+					"G. Las opciones correctas son la C y la D.",
+					"H. Las opciones correctas son la B, la C y la D."
+				],
+				"correctAnswer": "E. Las opciones correctas son la A y la D."
+			},
+			{
+				"question": "Diego Bernardini afirma que en la actualidad estamos observando cómo el concepto de longevidad, entendido como el tiempo de vida de una determinada especie, se está redefiniendo.",
+				"answers": [
+					"A. Es verdadero, porque la longevidad hoy comienza a ser una experiencia colectiva que la mayoría de nosotros viviremos.",
+					"B. Es falso, porque los cambios son culturales y no afectan cuestiones vinculadas al mundo biológico de la especie.",
+					"C. Es falso, porque la longevidad es hasta hoy en día un privilegio reservado para unos pocos.",
+					"D. Es verdadero, porque eso se combina con una cohorte de personas que son las menos educadas y más empobrecidas en la historia de la humanidad, y que por ello deben buscar formas de no repensar ni cambiar el tradicional paradigma de la nueva longevidad."
+				],
+				"correctAnswer": "A. Es verdadero, porque la longevidad hoy comienza a ser una experiencia colectiva que la mayoría de nosotros viviremos."
+			},
+			{
+				"question": "En el video en que Eduardo Levy Yeyati entrevista a Pablo Gerchunoff sobre la figura de Raúl Alfonsín, Gerchunoff señala que resultó fundamental el decreto del gobierno radical por el que permitió al Consejo Supremo de las Fuerzas Armadas, juzgar a las Juntas Militares.",
+				"answers": [
+					"A. Es falso, porque los militares argentinos recién comenzaron a ser juzgados durante el gobierno del Néstor Kirchner en 2003.",
+					"B. Es falso, porque hubo un solo juicio a las Juntas Militares y no fue el del Consejo Supremo de las Fuerzas Armadas.",
+					"C. Es verdadero, porque el decreto mencionado por el entrevistado les permitió a los militares ser absueltos en primera instancia y por ello acceder luego al indulto posterior que el mismo Alfonsín se viera obligado a dictar.",
+					"D. Es verdadero, porque si Alfonsín no hubiera firmado el decreto que enviaba a las Juntas Militares a ser juzgados primero por sus propios pares, no hubiera existido el famoso juicio posterior."
+				],
+				"correctAnswer": "D. Es verdadero, porque si Alfonsín no hubiera firmado el decreto que enviaba a las Juntas Militares a ser juzgados primero por sus propios pares, no hubiera existido el famoso juicio posterior."
+			},
+			{
+				"question": "Según Paola Bautista de Alemán, existen distintos tipos de tiranía, que se pueden diferenciar según su grado de intensidad autocrática.",
+				"answers": [
+					"A. Es falso, porque las tiranías son un régimen que se caracteriza solo por su forma autocrática de manejar el poder.",
+					"B. Es falso, porque en el siglo XXI ya no se discute que las tiranías son perjudiciales para el pueblo.",
+					"C. Es verdadero, porque se trata de una tarea que permite identificar sus formas, su alcance y, de alguna manera, su perversidad.",
+					"D. Es verdadero, porque el ejercicio por comprenderlas puede llevar a entender sus motivaciones y aumentar su valoración positiva."
+				],
+				"correctAnswer": "C. Es verdadero, porque se trata de una tarea que permite identificar sus formas, su alcance y, de alguna manera, su perversidad."
+			},
+			{
+				"question": "Según Romero, el presidente Carlos Menem sufrió la oposición de los militares por continuar las políticas del gobierno de Raúl Alfonsín y negarse a indultar a los miembros de las Juntas militares condenados por delitos cometidos durante la dictadura iniciada en 1976.",
+				"answers": [
+					"A. Es verdadero, ya que Menem había establecido sólidos contactos con un muy pequeño grupo nacionalista de militares, llamados carapintadas, y en especial, con su líder, el coronel Seineldín, y por ello se ganó la oposición del resto de las Fuerzas Armadas.",
+					"B. Es verdadero, ya que Menem llegó al poder por medio del partido peronista que históricamente había sido víctima de la represión ilegal llevada adelante por los militares entre 1976 y 1983 e, incluso Menem mismo, había sido apresado en esa época.",
+					"C. Es falso, porque dentro de su política más general de reconciliación con el pasado reciente, Menem indultó en diciembre de 1990 a los integrantes de las Juntas Militares, pese a la fuerte movilización popular en contra de la medida.",
+					"D. Es falso porque Menem ignoró la cuestión de los derechos humanos y las relaciones con el pasado militar, debido a que ya habían transcurrido varios años de la dictadura y además enfrentaba una grave situación económica a la que dedicó toda su gestión."
+				],
+				"correctAnswer": "C. Es falso, porque dentro de su política más general de reconciliación con el pasado reciente, Menem indultó en diciembre de 1990 a los integrantes de las Juntas Militares, pese a la fuerte movilización popular en contra de la medida."
+			},
+			{
+				"question": "Según Romero, durante el gobierno de Carlos Menem, la bonanza inicial ocultó por un tiempo los aspectos más duros de la gran transformación.",
+				"answers": [
+					"A. Es verdadero, porque la inflación no paró de crecer por la disimulada emisión monetaria.",
+					"B. Es verdadero, porque los efectos del ajuste estatal se disimularon al principio, por las importantes indemnizaciones pagadas, pero explotaron a partir de 1995.",
+					"C. Es falso, porque la convertibilidad fue exitosa y no tuvo contraindicaciones hasta el año 1999, en que Menem abandonó el poder.",
+					"D. Es falso, porque la Argentina venía de un duro escenario anterior y todo lo que pasó posteriormente pareció mucho más aceptable."
+				],
+				"correctAnswer": "B. Es verdadero, porque los efectos del ajuste estatal se disimularon al principio, por las importantes indemnizaciones pagadas, pero explotaron a partir de 1995."
+			},
+			{
+				"question": "Según Claudio Iglesias desde el 2011 la política económica argentina ha oscilado entre diferentes orientaciones y entre diferentes grupos políticos, sin poder impedir el paulatino deterioro de los niveles de vida de la mayoría de los argentinos.",
+				"answers": [
+					"A. Es falso, porque los planes quinquenales aplicados desde 2008 estaban dando resultados positivos, pero éstos fueron desechados por las sucesivas gestiones que fue ocupando el gobierno.",
+					"B. Es falso, porque si bien la economía argentina vivió un período de alta inflación, al mismo tiempo creció en forma record el PBI.",
+					"C. Es verdadero, porque produjo una falta de dinamismo de la economía y en bajas tasas de formación de capital y de generación de empleos.",
+					"D. Es verdadero, porque el neoliberalismo fue mermando la acción del Estado presente y el mercado fue convirtiéndose en el centro de las decisiones."
+				],
+				"correctAnswer": "C. Es verdadero, porque produjo una falta de dinamismo de la economía y en bajas tasas de formación de capital y de generación de empleos."
+			},
+			{
+				"question": "Según Romero, sorpresivamente, en noviembre de 1993, Menem y Alfonsín acordaron en secreto -el llamado 'Pacto de Olivos'- las condiciones para una reforma constitucional. Esto fue así, porque:",
+				"answers": [
+					"A. Incluiría la cláusula de reelección tan deseada por Menem.",
+					"B. Incluiría una serie de modificaciones impulsadas por el peronismo para reducir el margen de discrecionalidad en los nombramientos de jueces de la Corte Suprema de Justicia.",
+					"C. Incluiría una serie de modificaciones impulsadas por la UCR para modernizar el texto y reducir el margen de discrecionalidad presidencial.",
+					"D. Ambas partes acordaron la creación de las llamadas elecciones PASO (Primarias, Abiertas, Simultaneas y Obligatorias).",
+					"E. Las opciones correctas son la A y la B.",
+					"F. Las opciones correctas son la A y la C.",
+					"G. Las opciones correctas son la A y la D.",
+					"H. Las opciones correctas son la B, la C y la D."
+				],
+				"correctAnswer": "F. Las opciones correctas son la A y la C."
+			},
+			{
+				"question": "Gemma Galdón-Clavell explica en su video que por primera vez en la historia es más caro olvidar que recordar. Esto tiene efectos sociales como:",
+				"answers": [
+					"A. A los jóvenes de ahora le otorguemos una suerte de inmortalidad que será agradecida cuando sean mayores.",
+					"B. Que en la actualidad todo quede en registros digitales y eso pueda generar complicaciones a lo largo de la vida.",
+					"C. Haber logrado que la huella digital no tenga impacto en el futuro de las personas.",
+					"D. La penetración de aparatos tecnológicos en la video vigilancia tenga consecuencias que se deben investigar.",
+					"E. Las opciones correctas son la A y la B.",
+					"F. Las opciones correctas son la A y la C.",
+					"G. Las opciones correctas son la B y la D.",
+					"H. Las opciones correctas son la C y la D."
+				],
+				"correctAnswer": "G. Las opciones correctas son la B y la D."
+			},
+			{
+				"question": "Según Armando Chaguaceda y Eloy Viera en Cuba, como proceso socio-histórico y como orden político, no hay prácticamente avances –y sí mucho estancamiento y retrocesos– en lo referente a conquistas y derechos democratizadores. Esto se ve en que:",
+				"answers": [
+					"A. Un pequeño grupo de varios cientos de generales, gerentes y burócratas gobierna la isla caribeña desde hace seis décadas.",
+					"B. Un pequeño grupo capitaneado por un cerrado e inamovible núcleo familiar y militar ha ido proscribiendo toda forma de pluralismo y alternancia a sus once millones de habitantes.",
+					"C. Un pequeño grupo de revolucionarios otorgó a los isleños una dignidad que es más importante que la democracia.",
+					"D. El bloqueo de los Estados Unidos Cuba ha impedido la democratización de la isla.",
+					"E. Las opciones correctas son la A y la B.",
+					"F. Las opciones correctas son la B y la C.",
+					"G. Las opciones correctas son la C y la D.",
+					"H. Las opciones correctas son la A, la B y la D."
+				],
+				"correctAnswer": "E. Las opciones correctas son la A y la B."
+			},
+			{
+				"question": "En el video sobre la política del futuro, Julia Pomares afirma que hay importantes cambios que preocupan porque están llegando antes de que nos podamos ocupar de ellos. Por ejemplo, cómo están cambiando la política y las campañas electorales. Esto se ve en que:",
+				"answers": [
+					"A. Se reglamenta demasiado desde los Estados el uso de Facebook o WhatsApp para regular cómo se informa a los votantes.",
+					"B. Existen leyes anacrónicas que se limitan a regular cuánto se gasta en la televisión y en la radio.",
+					"C. La transformación más profunda de la política no va a estar en la tecnología sino en quiénes van a tomar decisiones.",
+					"D. La transformación más profunda de la política va a estar en el uso del voto electrónico.",
+					"E. Las opciones correctas son la A y la D.",
+					"F. Las opciones correctas son la B y la C.",
+					"G. Las opciones correctas son la C y la D.",
+					"H. Las opciones correctas son la A, la B y la D."
+				],
+				"correctAnswer": "F. Las opciones correctas son la B y la C."
+			},
+			{
+				"question": "Según Romero y como preludio a la crisis del 2001, la convertibilidad, sumada a diez años de inflación interna, tuvo como consecuencia un peso sobrevaluado. Esto generó que:",
+				"answers": [
+					"A. Se hiciera difícil competir en los mercados mundiales.",
+					"B. La impresión desmedida de billetes hiriera de muerte a la convertibilidad.",
+					"C. Pagar los vencimientos de la deuda requiriera un enorme esfuerzo fiscal y una severa reducción de los gastos del Estado.",
+					"D. Retrocedieran las exportaciones agrícola-ganaderas que habían sido uno de los pilares de la transformación de los noventa.",
+					"E. Las opciones correctas son la A y la C.",
+					"F. Las opciones correctas son la B y la C.",
+					"G. Las opciones correctas son la C y la D.",
+					"H. Las opciones correctas son la A, la B y la D."
+				],
+				"correctAnswer": "E. Las opciones correctas son la A y la C."
+			},
+			{
+				"question": "Según Tomás Straka, a diferencia de otros gobiernos que dominaron la región en la primera década del siglo XXI, como los de Lula Da Silva, Rafael Correa, Pepe Mujica y Evo Morales, el chavismo decidió llevar las cosas por un camino más radical. Esto fue así porque:",
+				"answers": [
+					"A. Tomó decisiones más temerarias en la administración de los recursos, pero, en proporción a aquellas apuestas, los resultados fueron más graves cuando las cosas no salieron como se esperaban.",
+					"B. Se trazó metas más altas e hizo más ruido internacional, pero, en proporción a aquellas apuestas, los resultados fueron más graves cuando las cosas no salieron como se esperaban.",
+					"C. Descontando los años del boom petrolero y los indicadores sociales que entonces hicieron aplaudir al mundo, el chavismo no logró ofrecer la estabilidad política, el crecimiento económico y la disminución de la pobreza que se vieron en otros países de la región.",
+					"D. Se trazó metas más altas e hizo más ruido internacional, y cuando, en proporción a aquellas apuestas, los resultados fueron mejores que los previstos, todo el poder quedó en manos de Chávez.",
+					"E. Las opciones correctas son la A y la D.",
+					"F. Las opciones correctas son la B y la C.",
+					"G. Las opciones correctas son la C y la D.",
+					"H. Las opciones correctas son la A, la B y la C."
+				],
+				"correctAnswer": "H. Las opciones correctas son la A, la B y la C."
+			},
+			{
+				"question": "Encabezada por Fernando de la Rúa, la coalición llamada Alianza por el Trabajo, la Justicia y la Educación, llegó al gobierno con una amplia confianza social, pero también con varios desafíos de difícil solución. Romero afirma que esto se produjo, entre otras razones, porque:",
+				"answers": [
+					"A. Todo parecía culpa de la convertibilidad, y eliminarla fue el nuevo reclamo colectivo y también la principal propuesta del nuevo gobierno.",
+					"B. El nuevo gobierno recibió una economía que estaba en recesión desde 1998.",
+					"C. La grieta ideológica con la oposición de izquierda imposibilitaba la concreción de acuerdos.",
+					"D. Todo reposaba sobre la convertibilidad, y mantenerla fue la nueva ilusión colectiva y también el principal respaldo del gobierno.",
+					"E. Las opciones correctas son la A y la B.",
+					"F. Las opciones correctas son la A y la D.",
+					"G. Las opciones correctas son la B y la D.",
+					"H. Las opciones correctas son la A, la B y la D."
+				],
+				"correctAnswer": "G. Las opciones correctas son la B y la D."
+			},
+			{
+				"question": "Según Sebastián Linares, las asambleas de ciudadanos sorteados se caracterizan por:",
+				"answers": [
+					"A. Estar sus miembros llamados a ocupar un lugar permanente en el sistema político democrático.",
+					"B. Tomar decisiones en un gran menú de temas, ya que la asamblea es soberana.",
+					"C. No estar llamadas a ocupar un lugar permanente en el sistema político democrático.",
+					"D. No sustituir a las elecciones realizadas de modo tradicional.",
+					"E. Las opciones correctas son la A y la B.",
+					"F. Las opciones correctas son la A y la C.",
+					"G. Las opciones correctas son la C y la D.",
+					"H. Las opciones correctas son la A, la C y la D."
+				],
+				"correctAnswer": "G. Las opciones correctas son la C y la D."
+			},
+			{
+				"question": "Julio Montero afirma que más allá de su larga trayectoria filosófica y de su influencia sobre varias constituciones occidentales de los siglos XVIII y XIX, la noción de derechos humanos recién alcanzó aceptación internacional plena durante la Segunda Guerra Mundial. Esto es así porque:",
+				"answers": [
+					"A. Hasta ese momento, había predominado la idea de que cada Estado tenía total libertad para fijar su propio marco legal.",
+					"B. Hasta ese momento, había predominado la idea de que solo la ONU podía intervenir si se violaban los derechos humanos de los ciudadanos de un país.",
+					"C. Hasta ese momento, había predominado la idea de que cada Estado tenía total libertad para decidir qué derechos les reconocía a sus ciudadanos.",
+					"D. Lo que los gobiernos hacían dentro de su territorio era un asunto de cada comunidad política y de nadie más.",
+					"E. Las opciones correctas son la A y la B.",
+					"F. Las opciones correctas son la B y la D.",
+					"G. Las opciones correctas son la A, la C y la D.",
+					"H. Las opciones correctas son la A, la B y la C."
+				],
+				"correctAnswer": "G. Las opciones correctas son la A, la C y la D."
+			},
+			{
+				"question": "Entre mediados del siglo XX e inicios del siglo XXI hubo en Argentina numerosos cambios de gobiernos y de régimen político. Entre ellos se puede mencionar:",
+				"answers": [
+					"A. El paso del gobierno de Raúl Alfonsín al de Carlos Menem no generó un cambio de régimen político a pesar de integrar ambos dos partidos diferentes.",
+					"B. El paso del gobierno de Adolfo Rodríguez Saá a Eduardo Duhalde no fue un cambio de régimen político, ya que estaba previsto en la Constitución Nacional.",
+					"C. El paso del gobierno de Fernando de la Rúa al de Carlos Menem no generó un cambio de régimen político.",
+					"D. La reelección de Duhalde no generó un cambio de régimen político porque fue intermediada por la reforma constitucional de 1994.",
+					"E. Las opciones correctas son la A y la B.",
+					"F. Las opciones correctas son la A y la C.",
+					"G. Las opciones correctas son la B y la D.",
+					"H. Las opciones correctas son la C y la D."
+				],
+				"correctAnswer": "E. Las opciones correctas son la A y la B."
+			},
+			{
+				"question": "Vale 1. Según Romero, de la Rúa convocó a Domingo Cavallo, el 'padre de la convertibilidad', transformado en la única esperanza de salvación para la ya desesperada opinión pública. Esto generó que:",
+				"answers": [
+					"A. Cavallo se convirtiera de hecho en un 'superministro', un papel adecuado a su personalidad.",
+					"B. En medio de una crisis social ya desbocada, Cavallo ensayara una solución no ortodoxa.",
+					"C. En medio de una crisis social ya desbocada, Cavallo ensayara una solución ortodoxa.",
+					"D. La nueva administración estadounidense, encabezada por George W. Bush, garantizara su apoyo al gobierno argentino.",
+					"E. Las opciones correctas son la A y la B.",
+					"F. Las opciones correctas son la A y la C.",
+					"G. Las opciones correctas son la A y la D.",
+					"H. Las opciones correctas son la B y la D."
+				],
+				"correctAnswer": "E. Las opciones correctas son la A y la B."
+			}
 		],
 	},
 ];
@@ -238,7 +465,7 @@ const allQuestionsData2 = [
 		year: 2023,
 		name: "PARTE A",
 		questions: [
-	
+
 		],
 	},
 ];
