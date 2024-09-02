62f74d9de19357e0c3cac3d467cce77217522635
Andresito <60946504+Andressitoito@users.noreply.github.com>
2024-09-02 18:24:53 -0300

    Inicio de Sección de IPC para Practicar Parciales
    e creó una nueva sección relacionada con IPC, iniciando así el segmento para practicar parciales de IPC.

--- src/app/page.js
+++ src/app/page.js
@@ -26,24 +26,27 @@ export default function Parciales() {
 		}
 	}, [fetchedData]); // Only re-run effect if fetchedData changes
 
-	const years = [2024];
 	return (
 		<>
 			<div className="flex flex-col items-center justify-center h-screen">
 				<h2 className="text-gray-300 italic text-3xl md:text-5xl mb-4 md:mb-8 font-bold text-center">
-					Prácticas<br className="hidden md:inline" /> Fáciles<br className="hidden md:inline" /> Exámenes<br className="hidden md:inline" /> ICSE
+					Parciales
+					<br className="hidden md:inline" />
+					Fáciles
 				</h2>
-				{years.map((year) => (
-					<div key={year + 1} className="w-48 mb-4">
-						<Link href={`/parciales/ICSE`}>
-							<div className="cursor-pointer bg-gray-800 hover:bg-gray-700 text-white py-2 px-4 rounded-lg transition duration-300 text-center">
-								{year}
-							</div>
-						</Link>
-					</div>
-				))}
-			</div>
 
+				<Link href={`/parciales/IPC`}>
+					<div className="cursor-pointer bg-gray-800 hover:bg-gray-700 text-white py-2 px-4 rounded-lg transition duration-300 text-center">
+						IPC
+					</div>
+				</Link>
+
+				<Link href={`/parciales/ICSE`}>
+					<div className="mt-2 cursor-pointer bg-gray-800 hover:bg-gray-700 text-white py-2 px-4 rounded-lg transition duration-300 text-center">
+						ICSE
+					</div>
+				</Link>
+			</div>
 		</>
 	);
 }
---
+++ src/app/parciales/IPC/page.js
@@ -0,0 +1,166 @@
+"use client";
+import Link from "next/link";
+import { allQuestionsData } from "./primer-cuatri/parciales.js";
+import { final_data } from "./final/final_data.js";
+import useStore from "../../../../store/store.js";
+import { useEffect, useState } from "react";
+import useCounterStore from "../../../../store/countersData.js";
+
+export default function Parciales() {
+	const setQuestionsData = useStore((state) => state.setQuestionsData);
+	const { countersData, setCountersData } = useCounterStore();
+	const [openIndex, setOpenIndex] = useState(null);
+	const [fetchedData, setFetchedData] = useState(false);
+
+	const handleButtonClick = (name) => {
+		const questionData = allQuestionsData.find((item) => item.name === name);
+		setQuestionsData(questionData);
+		localStorage.setItem("name", name);
+	};
+
+	const handleButtonFinal = (name) => {
+		const questionData = final_data.find((item) => item.name === name);
+		setQuestionsData(questionData);
+		localStorage.setItem("name", name);
+	};
+
+	const fetchData = async () => {
+		try {
+			const res = await fetch("/api/database/get_database_data");
+			const data = await res.json();
+			setCountersData(data.documents);
+			setFetchedData(true);
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		}
+	};
+
+	useEffect(() => {
+		if (!fetchedData) {
+			fetchData();
+		}
+	}, [fetchedData]);
+
+	useEffect(() => {
+		if (!countersData) {
+			fetchData();
+		}
+	}, [countersData]);
+
+	const toggleDiv = (index) => {
+		if (openIndex === index) {
+			setOpenIndex(null);
+		} else {
+			setOpenIndex(index);
+		}
+	};
+
+	return (
+		<>
+			<h2 className="text-white text-3xl mb-4 text-center mt-20">Parciales</h2>
+
+			<div className="flex flex-col items-center">
+				{/* ///////////////////////////////////////////*/}
+				{/* PRIMER CUATRIMESTRE INICIO */}
+				<div
+					className="w-48 mb-2 flex flex-col items-center"
+					onClick={() => toggleDiv(1)}
+				>
+					<div className="w-full cursor-pointer bg-gray-800 hover:bg-gray-700 text-white py-2 px-4 mb-3 rounded-lg transition duration-300 text-center disabled:opacity-50 cursor-not-allowed text-center">
+						1er cuatri
+					</div>
+
+					{openIndex === 1 && (
+						<div>
+							{allQuestionsData.map((item) => (
+								<div key={item.name} className="mb-4">
+									<Link
+										className="cursor-pointer bg-gray-700 hover:bg-gray-500 text-white py-2 px-4 rounded-lg transition duration-300 text-center"
+										href={{
+											pathname: "/parciales/IPC/primer-cuatri",
+										}}
+										onClick={() => handleButtonClick(item.name)}
+									>
+										{item.name}
+									</Link>
+								</div>
+							))}
+						</div>
+					)}
+				</div>
+				{/* PRIMER CUATRIMESTRE FIN */}
+				{/* ///////////////////////////////////////////*/}
+
+				{/* ///////////////////////////////////////////*/}
+				{/* SEGUNDO CUATRIMESTRE INICIO */}
+				<div
+					className="w-48 mb-2 flex flex-col items-center"
+					onClick={() => toggleDiv(2)}
+				>
+					<div className="w-full cursor-pointer bg-gray-800 hover:bg-gray-700 text-white py-2 px-4 mb-3 rounded-lg transition duration-300 text-center disabled:opacity-50 cursor-not-allowed text-center">
+						2do cuatri
+					</div>
+
+					{openIndex === 2 && <div>Proximamente...</div>
+					
+					// (
+					// 	<div>
+					// 		{allQuestionsData.map((item) => (
+					// 			<div key={item.name} className="mb-4">
+					// 				<Link
+					// 					className="cursor-pointer bg-gray-700 hover:bg-gray-500 text-white py-2 px-4 rounded-lg transition duration-300 text-center"
+					// 					href={{
+					// 						pathname: "/parciales/IPC/segundo-cuatri",
+					// 					}}
+					// 					onClick={() => handleButtonClick(item.name)}
+					// 				>
+					// 					{item.name}
+					// 				</Link>
+					// 			</div>
+					// 		))}
+					// 	</div>
+					// )
+					
+					}
+				</div>
+				{/* SEGUNDO CUATRIMESTRE FIN */}
+				{/* ///////////////////////////////////////////*/}
+
+				{/* FINAL INICIO */}
+				{/* ///////////////////////////////////////////*/}
+				<div
+					className="w-48 mb-2 flex flex-col items-center"
+					onClick={() => toggleDiv(3)}
+				>
+					<div className="w-full cursor-pointer bg-gray-800 hover:bg-gray-700 text-white py-2 px-4 mb-3 rounded-lg transition duration-300 text-center disabled:opacity-50 cursor-not-allowed text-center">
+						Final
+					</div>
+
+					{openIndex === 3 && <div>Proximamente...</div>
+					
+					// (
+					// 	<div>
+					// 		{final_data.map((item) => (
+					// 			<div key={item.name} className="mb-4">
+					// 				<Link
+					// 					className="cursor-pointer bg-gray-700 hover:bg-gray-500 text-white py-2 px-4 rounded-lg transition duration-300 text-center"
+					// 					href={{
+					// 						pathname: "/parciales/IPC/final",
+					// 					}}
+					// 					onClick={() => handleButtonFinal(item.name)}
+					// 				>
+					// 					{item.name}
+					// 				</Link>
+					// 			</div>
+					// 		))}
+					// 	</div>
+					// )
+					
+					}
+				</div>
+			</div>
+			{/* ///////////////////////////////////////////*/}
+			{/* FINAL FIN */}
+		</>
+	);
+}
